12d3ffe3e05f2248cfadf528b8f270331f3efc18
II-DW <dowon206@kakao.com>
2023-09-14 19:37:13 +0900

    fix: gitattributes

--- 자동 응답 봇/training.py
+++ 자동 응답 봇/training.py
@@ -32,18 +32,71 @@ for f_n in file_lst :
         pass
     
 komoran = Komoran()
-
-def pos(sentence):
-    jpype.attachThreadToJVM()
-    return komoran.pos(sentence)
-
 words = []
 classes = []
 documents = []
 
-for df in dfList :
-    for L in df :
-        print(L)
-    #if df['발화자'] == 'c' :
-        
+df = dfList[0]
+
+for i in range(len(df['발화문'])) :
+    try :
+        Text = df['발화문'][i]
+        cartegory = df['인텐트'][i]
+        word_list = komoran.pos(Text)
+        w_d_list = []
+        for word in word_list :
+            w_d_list.append(word[0])
+        words.extend(w_d_list)
+        if cartegory not in classes :
+            classes.append(cartegory)
+        documents.append((w_d_list, cartegory))
+    except Exception as e :
+        print(e, "라는 이유로 오류가 났습니다.")
+        print("해당 구문은 이와 같습니다.")
+        print("-------------------------------------------------------------------------------")
+        print(df['발화문'][i]) 
+        print("-------------------------------------------------------------------------------")
+
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)  # [0, 0, 0, 0, 0, 0, 0]
+
+for document in documents:
+  bag = []
+  word_patterns = document[0]
+  for word in words:
+    if word in word_patterns:
+      bag.append(1)
+    else:
+      bag.append(0)
+
+  output_row = list(output_empty)
+  output_row[classes.index(document[1])] = 1  # 원핫 벡터  [0, 0, 0, 0, 1, 0, 0]
+  training.append([bag, output_row])
+
+
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]), ), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+history = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', history)
+print("Done")
+
 
